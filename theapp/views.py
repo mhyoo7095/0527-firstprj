b2e570ada788f8134a2c3ca1b7be253ef23e9af3
mhyoo7095 <mhyoo7095@gmail.com>
2020-05-27 19:56:00 +0900

    create firstprj

---
+++ theapp/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def main(requests):
+    return render(requests, 'main.html')
+    
+def photos(requests):
+    return render(requests, 'photos.html')
+    
+def intro(requests):
+    return render(requests, 'intro.html')
+
+def user(requests):
+    return render(requests, 'user.html')
+
